ae5690352bb088bb4b02d631143f881cda06316c
Jemy <jemygraw@gmail.com>
2018-06-19 22:41:14 +0800

    refractor the code to create logging appender workflow

--- src/main/java/com/qiniu/log4j/Log4j2QiniuAppender.java
+++ src/main/java/com/qiniu/log4j/Log4j2QiniuAppender.java
@@ -1,6 +1,5 @@
 package com.qiniu.log4j;
 
-import com.qiniu.pandora.common.Config;
 import com.qiniu.pandora.common.PandoraClient;
 import com.qiniu.pandora.common.PandoraClientImpl;
 import com.qiniu.pandora.common.QiniuException;
@@ -8,12 +7,7 @@ import com.qiniu.pandora.http.Response;
 import com.qiniu.pandora.pipeline.points.Batch;
 import com.qiniu.pandora.pipeline.points.Point;
 import com.qiniu.pandora.util.Auth;
-import com.qiniu.target.common.Analyzer;
-import com.qiniu.target.common.ValueType;
-import com.qiniu.target.common.WFStatus;
-import com.qiniu.target.common.Whence;
-import com.qiniu.target.logdb.LogdbClient;
-import com.qiniu.target.pipeline.*;
+import com.qiniu.target.common.Configs;
 import com.qiniu.target.sender.LogSender;
 import org.apache.logging.log4j.core.Filter;
 import org.apache.logging.log4j.core.Layout;
@@ -26,8 +20,6 @@ import org.apache.logging.log4j.core.config.plugins.PluginFactory;
 
 import java.io.Serializable;
 import java.io.UnsupportedEncodingException;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -41,8 +33,7 @@ import java.util.concurrent.locks.ReentrantLock;
  * Based on log4j version 2.x
  */
 @Plugin(name = "Log4j2QiniuAppender", category = "Core", elementType = "appender", printObject = true)
-public class Log4j2QiniuAppender extends AbstractAppender implements ValueType, Analyzer, Whence, WFStatus {
-    private static int DefaultAutoFlushInterval = 5;//5 seconds
+public class Log4j2QiniuAppender extends AbstractAppender implements Configs {
     private Lock rwLock;
     private Batch batch;
     private ExecutorService executorService;
@@ -173,94 +164,35 @@ public class Log4j2QiniuAppender extends AbstractAppender implements ValueType, 
     @PluginFactory
     public static Log4j2QiniuAppender createAppender(@PluginAttribute("name") String name,
                                                      @PluginAttribute("workflowName") String workflowName,
+                                                     @PluginAttribute("workflowRegion") String workflowRegion,
                                                      @PluginAttribute("pipelineRepo") String pipelineRepo,
                                                      @PluginAttribute("logdbRepo") String logdbRepo,
+                                                     @PluginAttribute("logdbRetention") String logdbRetention,
                                                      @PluginAttribute("accessKey") String accessKey,
                                                      @PluginAttribute("secretKey") String secretKey,
                                                      @PluginAttribute("autoFlushInterval") int autoFlushInterval,
                                                      @PluginElement("filter") final Filter filter,
                                                      @PluginElement("layout") Layout<? extends Serializable> layout,
                                                      @PluginAttribute("ignoreExceptions") boolean ignoreExceptions) {
+
         Auth auth = Auth.create(accessKey, secretKey);
         PandoraClient client = new PandoraClientImpl(auth);
-        PipelineClient pipelineClient = new PipelineClient(client);
-        LogdbClient logdbClient = new LogdbClient(client);
+
+        //check attributes
+        if (workflowRegion == null || workflowRegion.isEmpty()) {
+            workflowRegion = DefaultWorkflowRegion;
+        }
+        if (logdbRetention == null || logdbRetention.isEmpty()) {
+            logdbRetention = DefaultLogdbRetention;
+        }
+
+        //try to create appender workflow
         try {
-            //check workflow
-            if (!pipelineClient.workflowExists(workflowName)) {
-                CreateWorkflowInput createWorkflowInput = new CreateWorkflowInput();
-                createWorkflowInput.workflowName = workflowName;
-                createWorkflowInput.region = Config.region;
-                pipelineClient.createWorkflow(createWorkflowInput);
-            }
-
-            //check pipeline
-            if (!pipelineClient.repoExists(pipelineRepo)) {
-                CreateRepoInput createRepoInput = new CreateRepoInput();
-                createRepoInput.workflowName = workflowName;
-                createRepoInput.region = Config.region;
-                createRepoInput.schema = new RepoSchemaEntry[]{
-                        new RepoSchemaEntry("timestamp", TypeLong, true),
-                        new RepoSchemaEntry("level", TypeString, true),
-                        new RepoSchemaEntry("logger", TypeString, true),
-                        new RepoSchemaEntry("marker", TypeString, true),
-                        new RepoSchemaEntry("message", TypeString, true),
-                        new RepoSchemaEntry("thread_name", TypeString, true),
-                        new RepoSchemaEntry("thread_id", TypeLong, true),
-                        new RepoSchemaEntry("thread_priority", TypeLong, true),
-                        new RepoSchemaEntry("exception", TypeString, true),
-                };
-                pipelineClient.createRepo(pipelineRepo, createRepoInput);
-            }
-
-            //check logdb
-            if (!logdbClient.repoExists(logdbRepo)) {
-                com.qiniu.target.logdb.CreateRepoInput createRepoInput = new com.qiniu.target.logdb.CreateRepoInput();
-                createRepoInput.region = "nb";
-                createRepoInput.retention = "30d";
-                createRepoInput.schema = new com.qiniu.target.logdb.RepoSchemaEntry[]{
-                        new com.qiniu.target.logdb.RepoSchemaEntry("timestamp", TypeDate),
-                        new com.qiniu.target.logdb.RepoSchemaEntry("level", TypeString, KeyWordAnalyzer),
-                        new com.qiniu.target.logdb.RepoSchemaEntry("logger", TypeString, KeyWordAnalyzer),
-                        new com.qiniu.target.logdb.RepoSchemaEntry("marker", TypeString, KeyWordAnalyzer),
-                        new com.qiniu.target.logdb.RepoSchemaEntry("message", TypeString, StandardAnalyzer),
-                        new com.qiniu.target.logdb.RepoSchemaEntry("thread_name", TypeString, StandardAnalyzer),
-                        new com.qiniu.target.logdb.RepoSchemaEntry("thread_id", TypeLong),
-                        new com.qiniu.target.logdb.RepoSchemaEntry("thread_priority", TypeLong),
-                        new com.qiniu.target.logdb.RepoSchemaEntry("exception", TypeString, StandardAnalyzer),
-                };
-                logdbClient.createRepo(logdbRepo, createRepoInput);
-            }
-
-            //check export
-            String exportName = String.format("%s_export_to_%s", pipelineRepo, logdbRepo);
-            if (!pipelineClient.exportExists(pipelineRepo, exportName)) {
-                CreateExportInput<ExportLogdbSpec> createExportInput = new CreateExportInput<ExportLogdbSpec>();
-                createExportInput.whence = WHENCE_NEWEST;
-                createExportInput.type = "logdb";
-                Map<String, Object> exportFields = new HashMap<String, Object>();
-                exportFields.put("timestamp", "#timestamp");
-                exportFields.put("level", "#level");
-                exportFields.put("logger", "#logger");
-                exportFields.put("marker", "#marker");
-                exportFields.put("message", "#message");
-                exportFields.put("thread_name", "#thread_name");
-                exportFields.put("thread_id", "#thread_id");
-                exportFields.put("thread_priority", "#thread_priority");
-                exportFields.put("exception", "#exception");
-                createExportInput.spec = new ExportLogdbSpec(logdbRepo, exportFields, true, true);
-                pipelineClient.createExport(pipelineRepo, exportName, createExportInput);
-            }
-
-            //start workflow
-            WorkflowStatus workflowStatus = pipelineClient.workflowStatus(workflowName);
-            if (!workflowStatus.status.equals(WORKFLOW_STARTED)) {
-                pipelineClient.startWorkflow(workflowName);
-            }
+            QiniuAppenderClient.createAppenderWorkflow(client, workflowName, workflowRegion,
+                    pipelineRepo, logdbRepo, logdbRetention);
         } catch (Exception e) {
             e.printStackTrace();
-            //logging based on Qiniu initialization failed
-            return null;
+            return null;//logging appender initialization failed
         }
 
         return new Log4j2QiniuAppender(name, filter, layout, ignoreExceptions, pipelineRepo, client, autoFlushInterval);
