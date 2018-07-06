bdeb1c03b84906697f5c863fb4e564e9c3042b01
Jin XinXin <jinxinxin@qiniu.com>
2018-07-06 15:13:00 +0800

    [ISSUE-4] separate the distribution of different log library dependencies

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,28 @@
 export PROJ_HOME=/Users/jemy/Developer/JavaProjects/qiniu-logging-plugin
 export VERSION=1.0.0
-cd $PROJ_HOME/log4j && mvn package 
-cd $PROJ_HOME/log4j2 && mvn package
+
+# mkdir 
+mkdir -p $PROJ_HOME/deploy/log4j/
+mkdir -p $PROJ_HOME/deploy/log4j2/
+mkdir -p $PROJ_HOME/deploy/logback/
+mkdir -p $PROJ_HOME/deploy/output/
+
+# mvn package
+cd $PROJ_HOME/log4j   && mvn package 
+cd $PROJ_HOME/log4j2  && mvn package
 cd $PROJ_HOME/logback && mvn package
-cp $PROJ_HOME/log4j/target/qiniu-logging-plugin-log4j-$VERSION.jar $PROJ_HOME/deploy
-cp $PROJ_HOME/log4j2/target/qiniu-logging-plugin-log4j2-$VERSION.jar $PROJ_HOME/deploy
-cp $PROJ_HOME/logback/target/qiniu-logging-plugin-logback-$VERSION.jar $PROJ_HOME/deploy
+
+# collect jars
+cp $PROJ_HOME/log4j/target/qiniu-logging-plugin-log4j-$VERSION.jar     $PROJ_HOME/deploy/log4j/
+cp $PROJ_HOME/log4j2/target/qiniu-logging-plugin-log4j2-$VERSION.jar   $PROJ_HOME/deploy/log4j2/
+cp $PROJ_HOME/logback/target/qiniu-logging-plugin-logback-$VERSION.jar $PROJ_HOME/deploy/logback/
+
+# tar gz packages
+cd $PROJ_HOME/deploy/log4j/   && tar czvf qiniu-logging-plugin-log4j-$VERSION.tar.gz *.jar
+cd $PROJ_HOME/deploy/log4j2/  && tar czvf qiniu-logging-plugin-log4j2-$VERSION.tar.gz *.jar
+cd $PROJ_HOME/deploy/logback/ && tar czvf qiniu-logging-plugin-logback-$VERSION.tar.gz *.jar
+
+# move files to output
+mv $PROJ_HOME/deploy/log4j/qiniu-logging-plugin-log4j-$VERSION.tar.gz     $PROJ_HOME/deploy/output/
+mv $PROJ_HOME/deploy/log4j2/qiniu-logging-plugin-log4j2-$VERSION.tar.gz   $PROJ_HOME/deploy/output/
+mv $PROJ_HOME/deploy/logback/qiniu-logging-plugin-logback-$VERSION.tar.gz $PROJ_HOME/deploy/output/
